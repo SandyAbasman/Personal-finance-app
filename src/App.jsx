e74a2f8e02b8411b8a9604e869316569c6f703be
Sandy Abasman <abasifrekesandy@gmail.com>
2024-08-26 22:18:45 +0400

    fixing conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <p>personal finance app</p>
+      <p className="text-red-950 text-lg">personal new finance app
+
+        
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
