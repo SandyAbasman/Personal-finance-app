0a857d80e2fb5defabd95b73be2ef79bb232c3c1
sandygudie <goodnewssandy@gmail.com>
2024-08-27 13:06:32 +0100

    Feature:Added Husky and lint-staged for pre-commit hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import './App.css'
 
-
 function App() {
   return (
     <div>
-      <p className="text-red-950 text-lg">personal new finance app
-
-        
-      </p>
+      <p className="text-red-950 text-lg">personal new finance app</p>
     </div>
   )
 }
