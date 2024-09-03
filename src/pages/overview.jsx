66636bdf36f88df90ce9611bf5847c6b075ca891
Sandy Abasman <abasifrekesandy@gmail.com>
2024-09-03 12:04:31 +0400

    fixed hover changes

--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CurrentBalance from '../component/CurrentBalance'
+import Balance from '../component/Balance'
 import data from '../utilis/data.json'
 import Pot from '../component/Pot'
 
@@ -8,9 +8,9 @@ export default function Overview() {
     <div className="w-full h-auto flex flex-col gap-3 p-4">
       <h2 className="text-blue font-bold pb-8  text-2xl">Overview</h2>
       <div className=" flex flex-wrap  flex-row gap-6">
-        <CurrentBalance heading="Current balance" amount={data.balance.current} />
-        <CurrentBalance heading="Income" amount={data.balance.income} />
-        <CurrentBalance heading="Income" amount={data.balance.expenses} />
+        <Balance heading="Current balance" amount={data.balance.current} />
+        <Balance heading="Income" amount={data.balance.income} />
+        <Balance heading="Income" amount={data.balance.expenses} />
       </div>
 
       <div className="w-1/2 h-auto">
