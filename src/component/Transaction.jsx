c3567d9ea5796e67d920e71d7f8110221941fccc
sandygudie <goodnewssandy@gmail.com>
2024-09-08 20:47:22 +0100

    Added update

--- src/component/Transaction.jsx
+++ src/component/Transaction.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UpperBox from './UpperBox'
 import data from '../utilis/data.json'
+import dayjs from 'dayjs'
 
 export default function Transaction() {
   return (
@@ -9,15 +10,17 @@ export default function Transaction() {
 
       {data.transactions.slice(0, 5).map((trans) => {
         return (
-          <div key={trans.name} className="flex  justify-between  item-center sm:gap-2 flex-row">
+          <div key={trans.name} className="flex justify-between item-center sm:gap-2 flex-row">
             <div className="flex flex-row justify-start gap-2 items-center">
               <img className="w-6 h-6 rounded-full" src={trans.avatar} alt={trans.name} />
               <p className="text-nowrap text-sm text-blue font-semibold">{trans.name}</p>
             </div>
 
             <div className="flex flex-col justify-start items-start md:gap-1 ">
-              <p className="text-xs font-bold ">${trans.amount}</p>
-              <p className="text-xs text-blue/50">date</p>
+              <p className="text-xs font-bold">
+                {trans.amount > 0 ? `+$${Math.abs(trans.amount)}` : `-$${Math.abs(trans.amount)}`}
+              </p>
+              <p className="text-xs text-blue/50">{dayjs(trans.date).format('DD MMM YYYY')}</p>
             </div>
           </div>
         )
