737269360870a72dac7918f295270d4918401b1b
Sandy Abasman <abasifrekesandy@gmail.com>
2024-09-14 12:37:06 +0400

    transaction layout created with responsive input field

--- src/component/InputFieldTop.jsx
+++ src/component/InputFieldTop.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
 import SearchIcon from '/assets/images/search-icon.svg'
+import ListDownIcon from '/assets/images/icon-caret-down.svg'
 
 export default function InputFieldTop() {
   return (
-    <div className="flex justify-between items-center self-stretch">
+    <div className=" w-full h-auto flex justify-between gap-300 items-center self-stretch">
       <SearchInputField />
+
+      <LeftInputFieldTop>
+        <SortBy />
+        <Category />
+      </LeftInputFieldTop>
     </div>
   )
 }
 
 function SearchInputField() {
   return (
-    <div className="flex flex-row relative">
+    <div className="flex   flex-row items-start relative  gap-50">
       <input
         type="text"
         placeholder="Search transaction"
-        className="  px-[1.25rem] py-[0.2rem] rounded-md  border"
+        className=" w-[320px] px-250 py-150 rounded-lg bg-white border-background flex items-start gap-200 self-stretch  border"
       />
-      <img src={SearchIcon} alt="search icon" className="absolute right-1 top-2" />
+      <img
+        src={SearchIcon}
+        alt="search icon"
+        className=" flex-shrink-0 absolute w-[1.5rem] h-[1.5rem] right-3 text-gray-900 opacity-50 top-4 "
+      />
+    </div>
+  )
+}
+
+function LeftInputFieldTop({ children }) {
+  return <div className="md:flex hidden items-center justify-end gap-300 ">{children}</div>
+}
+
+function SortBy() {
+  return (
+    <div className="flex items-center gap-100">
+      <p className="text-gray-500 text-[14px] style-normal font-[400]">Sort by</p>
+      <div className="flex  px-150 py-250 item-center self-stretch gap-200  border border-#98908B rounded-md">
+        <p className="text-[14px] font-[400]">Latest</p>
+        <img src={ListDownIcon} alt="icon down" />
+      </div>
+    </div>
+  )
+}
+
+function Category() {
+  return (
+    <div className="flex items-center gap-100">
+      <p className="text-gray-500 text-[14px] style-normal font-[400]">Category</p>
+      <div className="flex  px-150 py-250 item-center self-stretch gap-200  border border-#98908B rounded-md">
+        <p className="text-[14px] font-[400]">All Transactions</p>
+        <img src={ListDownIcon} alt="icon down" />
+      </div>
     </div>
   )
 }
--- src/pages/transaction.jsx
+++ src/pages/transaction.jsx
@@ -3,10 +3,12 @@ import InputFieldTop from '../component/InputFieldTop'
 
 export default function Transaction() {
   return (
-    <div className="w-full h-auto flex flex-col gap-3 p-4">
-      <h2 className="text-blue font-bold mb-1  text-2xl">Transaction</h2>
+    <div className="w-full h-auto flex flex-col bg-background gap-400 md:px-400 md:py-500 px-300 py-200 flex-1 ">
+      <h2 className="text-grey-900  font-[700]   md:w-full max-w-30 px-100 py-0  text-[32px] ">
+        Transaction
+      </h2>
 
-      <div className=" w-full h-auto flex md:flex-nowrap justify-between gap-2 md:flex-row bg-white  flex-col ">
+      <div className=" w-full h-auto flex flex-col gap-300 md:px-400 md:py-400 px-300 py-250  items-start self-stretch bg-white rounded-lg">
         <InputFieldTop />
       </div>
     </div>
